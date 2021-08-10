3365614f15ec5d4c14f7e41385e38601f5b4fc83
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2021-08-11 02:07:54 +0600

    add about more

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 const About = () => {
-    return (
-        <div>
-            about
-        </div>
-    );
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/about.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
